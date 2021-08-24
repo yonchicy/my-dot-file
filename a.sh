4c41492fb410837d275d3f6baafe69f66b94e607
yonchicy <1079279184@qq.com>
2021-08-24 22:10:16 +0800

    add sh

---
+++ a.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/jeffreytse/zsh-vi-mode.git
+git clone https://github.com/romkatv/powerlevel10k.git
