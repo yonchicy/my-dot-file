6241d6bb22a2fb3056528d2ddf74e94232573a90
yonchicy <1079279184@qq.com>
2021-10-10 17:18:59 +0800

    完善说明文档

---
+++ zsh-plug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://hub.fastgit.org/zsh-users/zsh-autosuggestions.git
+git clone https://hub.fastgit.org/zsh-users/zsh-syntax-highlighting.git
+git clone https://hub.fastgit.org/jeffreytse/zsh-vi-mode.git
+git clone https://hub.fastgit.org/romkatv/powerlevel10k.git
+git clone https://hub.fastgit.org/wting/autojump.git
