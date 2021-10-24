ebb67e0d58c5640c199c11e8ad9cfd3312554790
yonchicy <1079279184@qq.com>
2021-10-24 17:29:09 +0800

    修复gruvbox对hop的颜色支持

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -26,9 +26,6 @@ vo.ignorecase=true
 vo.smartcase=true
 
 
-vim.cmd('hi HopNextKey guibg=#fabd2f guifg=#000000')
-vim.cmd('hi HopNextKey1 guibg=#fabd2f guifg=#000000')
-vim.cmd('hi HopUnmatched guifg=#928374' )
 vim.api.nvim_exec([[
 au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=150, on_visual=true}
 ]],false)
