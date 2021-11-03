30e4486bda8848aa70969c3006a31824ada96287
yonchicy <1079279184@qq.com>
2021-11-03 21:06:22 +0800

    feat 让选择模式下高亮变得明显

--- nvim/lua/plug_set/colorscheme.lua
+++ nvim/lua/plug_set/colorscheme.lua
@@ -6,9 +6,9 @@ vim.g.gruvbox_material_background="hard"
 vim.g.gruvbox_material_palette="original"
 vim.o.termguicolors=true
 vim.cmd('hi LineNr guifg=#DFFF00')
-vim.cmd('colorscheme gruvbox')
+vim.cmd('colorscheme onedark')
 vim.cmd('hi Search guifg=#ff9800 guibg=#000000' )
-vim.cmd('hi Visual guibg=#46413f ' )
+vim.cmd('hi Visual cterm=reverse gui=reverse' )
 vim.cmd('hi HopNextKey guibg=#fabd2f guifg=#000000')
 vim.cmd('hi HopNextKey1 guibg=#fabd2f guifg=#000000')
 vim.cmd('hi HopUnmatched guifg=#928374' )
