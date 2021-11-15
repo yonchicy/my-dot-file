94337830f4cab6ef5a67fc6c727b4832e5408ffe
yonchicy <1079279184@qq.com>
2021-11-15 12:21:11 +0800

    修复快捷键的一些bug

--- nvim/lua/plug_set/trouble.lua
+++ nvim/lua/plug_set/trouble.lua
@@ -57,6 +57,9 @@ vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>TroubleToggle lsp_workspace_dia
 vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>TroubleToggle lsp_document_diagnostics<cr>",
   {silent = true, noremap = true}
 )
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>TroubleToggle lsp_document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
 vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
   {silent = true, noremap = true}
 )
