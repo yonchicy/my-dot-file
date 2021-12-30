281536c8f63993760053ff2a0ad75b95269b70a3
yonchicy <1079279184@qq.com>
2021-12-30 21:36:30 +0800

    add cmp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -32,19 +32,23 @@
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm {
+      ["<Tab>"] = cmp.mapping.confirm {
          behavior = cmp.ConfirmBehavior.Replace,
          select = true,
       },
-      ["<Tab>"] = function(fallback)
-         if cmp.visible() then
-            cmp.select_next_item()
-         elseif require("luasnip").expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-         else
-            fallback()
-         end
-      end,
+      ["<cr>"] = cmp.mapping.confirm {
+         behavior = cmp.ConfirmBehavior.Replace,
+         select = true,
+      },
+      -- ["<Tab>"] = function(fallback)
+      --    if cmp.visible() then
+      --       cmp.select_next_item()
+      --    elseif require("luasnip").expand_or_jumpable() then
+      --       vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+      --    else
+      --       fallback()
+      --    end
+      -- end,
       ["<S-Tab>"] = function(fallback)
          if cmp.visible() then
             cmp.select_prev_item()
