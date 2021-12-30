e30a2dd7fbf87fc2996e7b43fcd1e5209d7817af
yonchicy <1079279184@qq.com>
2021-12-30 13:30:22 +0800

    update cmp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -22,42 +22,45 @@
   cmp.setup({
     snippet = {
       expand = function(args)
-        -- For `vsnip` user.
-
-        -- For `luasnip` user.
         require('luasnip').lsp_expand(args.body)
-
-        -- For `ultisnips` user.
-        -- vim.fn["UltiSnips#Anon"](args.body)
       end,
     },
-
     mapping = {
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-	  ['<C-Space>'] = cmp.mapping.complete(),
-	  ['<C-e>'] = cmp.mapping.close(),
-	  ['<Tab>'] = cmp.mapping.confirm({
-		  select = true ,
-		  behavior=cmp.ConfirmBehavior.Replace
-	  }),
-	  -- ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(),{
-		  -- 'i','s'
-	  -- }),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm {
+         behavior = cmp.ConfirmBehavior.Replace,
+         select = true,
+      },
+      ["<Tab>"] = function(fallback)
+         if cmp.visible() then
+            cmp.select_next_item()
+         elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+         else
+            fallback()
+         end
+      end,
+      ["<S-Tab>"] = function(fallback)
+         if cmp.visible() then
+            cmp.select_prev_item()
+         elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+         else
+            fallback()
+         end
+      end,
     },
     sources = {
       { name = 'nvim_lsp' },
-      { name = 'orgmode' },
-
-      -- For vsnip user.
-
-      -- For luasnip user.
       { name = 'luasnip' },
-
-      -- For ultisnips user.
-      -- { name = 'ultisnips' },
-
       { name = 'buffer' },
+      { name = 'path' },
+      { name = 'nvim_lua' },
     }
   })
 
