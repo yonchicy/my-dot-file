1b8f9d1bb26a6520b2ef315c7c99dfe694c342e5
yonchicy <1079279184@qq.com>
2022-04-11 16:43:32 +0800

    fix typo

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -171,7 +171,7 @@ M.harpoon = function ()
     vim.api.nvim_set_keymap("n", "mf", "<cmd>lua require('harpoon.mark').add_file()<cr>",
     {silent = true, noremap = true}
     )
-    vim.api.nvim_set_keymap("n", "<leader>mm", "<cmd>lua require('karpoon.ui').toggle_quick_menu()<cr>",
+    vim.api.nvim_set_keymap("n", "<leader>mm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
     {silent = true, noremap = true}
     )
     -- vim.api.nvim_set_keymap("v", "<leader>tt", "<cmd>SymbolsOutline<cr>",
