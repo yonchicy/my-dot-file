32cf51d3fb1f14e24add72873d97b46367b49b73
yonchicy <1079279184@qq.com>
2022-10-27 16:14:16 +0800

    update gitui and nvim smooth scroll

--- nvchad_custom/plugins/init.lua
+++ nvchad_custom/plugins/init.lua
@@ -1,80 +1,79 @@
-local overrides = require "custom.plugins.overrides"
+local overrides = require("custom.plugins.overrides")
 
 return {
-  ["nvim-treesitter/nvim-treesitter-textobjects"] = {
-    after = "nvim-treesitter"
-  },
-  ["goolord/alpha-nvim"] =false,
-  ["ggandor/lightspeed.nvim"] = {
-    event = "BufRead",
-  },
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.plugins.null-ls"
-    end,
-  },
+	["nvim-treesitter/nvim-treesitter-textobjects"] = {
+		after = "nvim-treesitter",
+	},
+	["goolord/alpha-nvim"] = false,
+	["ggandor/lightspeed.nvim"] = {
+		event = "BufRead",
+	},
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 
-  ["hrsh7th/nvim-cmp"] = {
-    after = "friendly-snippets",
-    config = function()
-      require "custom.plugins.cmp"
-    end,
-  },
-  -- lsp enhance
-  ["simrat39/symbols-outline.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function ()
-      require("symbols-outline").setup()
-    end
-  },
-  ["folke/trouble.nvim"] = {
-    after = "nvim-lspconfig",
-  },
-  ["Pocco81/AutoSave.nvim"] = {
-    config = function()
-      require("auto-save").setup()
-    end,
-  },
-  ["tpope/vim-repeat"] = {
+	["hrsh7th/nvim-cmp"] = {
+		after = "friendly-snippets",
+		config = function()
+			require("custom.plugins.cmp")
+		end,
+	},
+	-- lsp enhance
+	["simrat39/symbols-outline.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+	["folke/trouble.nvim"] = {
+		after = "nvim-lspconfig",
+	},
+	["Pocco81/AutoSave.nvim"] = {
+		config = function()
+			require("auto-save").setup()
+		end,
+	},
+	["tpope/vim-repeat"] = {},
+	["kylechui/nvim-surround"] = {
+		tag = "*",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
+	["karb94/neoscroll.nvim"] = {
+		event = "WinScrolled",
+		config = function()
+			require("custom.plugins.neoscroll")
+		end,
+	},
+	["p00f/nvim-ts-rainbow"] = {},
+	-- dev
+	["folke/lua-dev.nvim"] = {},
+	["rmagatti/goto-preview"] = {
+		config = function()
+			require("custom.plugins.goto-preview")
+		end,
+	},
 
-  },
-  ["kylechui/nvim-surround"] = {
-    tag = "*",
-    config = function ()
-      require("nvim-surround").setup()
-    end
-  },
-  ["p00f/nvim-ts-rainbow"] = {
-
-  },
-  -- dev
-  ["folke/lua-dev.nvim"] = {
-
-  },
-  ["rmagatti/goto-preview"] = {
-    config = function ()
-      require "custom.plugins.goto-preview"
-    end
-  },
-
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter
-  },
-  ["williamboman/mason.nvim"] = {
-    override_options = overrides.mason
-  },
-  ["kyazdani42/nvim-tree.lua"] ={
-    override_options = overrides.nvimtree,
-  },
-  ["lewis6991/gitsigns.nvim"] = {
-    override_options = overrides.gitsigns,
-  },
-
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = overrides.treesitter,
+	},
+	["williamboman/mason.nvim"] = {
+		override_options = overrides.mason,
+	},
+	["kyazdani42/nvim-tree.lua"] = {
+		override_options = overrides.nvimtree,
+	},
+	["lewis6991/gitsigns.nvim"] = {
+		override_options = overrides.gitsigns,
+	},
 }
