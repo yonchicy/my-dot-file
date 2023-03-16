ce0bb3a8303dce67a0181ef5ac4ac0138d6332d1
yonchicy <1079279184@qq.com>
2023-03-16 22:11:13 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 mkdir -p ~/.config
-cp  ~/my-dot-file/zshrc_double_system ~/.zshrc
-ln -s ~/my-dot-file/nvim ~/.config/nvim
-mkdir -p ~/.zsh
-cp zsh-plug.sh ~/.zsh
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+cp  ~/my-dot-file/.zshrc ~/.zshrc
+cp  ~/my-dot-file/.vimrc ~/.vimrc
+cp  $HOME/my-dot-file/config/* ~/.config/ -r
+rm -rf $HOME/.config/nvim 
+ln -s $HOME/my-dot-file/LazyVim $HOME/.config/nvim
+mkdir -p $HOME/.zsh
+cp $HOME/my-dot-file/zsh-plug.sh $HOME/.zsh
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+
+# install starship
+curl -sS https://starship.rs/install.sh | sh
+git clone https://github.com/neovim/neovim.git
+
+cd $HOME/.zsh && bash zsh-plug.sh
--- zsh-plug.sh
+++ zsh-plug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/jeffreytse/zsh-vi-mode.git
-git clone https://github.com/romkatv/powerlevel10k.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
+git clone https://github.com/jeffreytse/zsh-vi-mode.git 
+git clone https://github.com/romkatv/powerlevel10k.git 
 git clone https://github.com/skywind3000/z.lua.git
