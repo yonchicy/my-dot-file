5d461c53d1792389afd54000f66269ae48acd9c2
yonchicy <1079279184@qq.com>
2023-04-04 14:40:11 +0800

    处理clangd offsetEncoding的问题

--- nvchad_custom/configs/lspconfig.lua
+++ nvchad_custom/configs/lspconfig.lua
@@ -1,13 +1,18 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
-local servers = {  "clangd","lua_ls","rust_analyzer" }
+local servers = { "clangd", "lua_ls", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	if lsp == "clangd" then
+		capabilities.offsetEncoding = "utf-16"
+	else
+		capabilities.offsetEncoding = "utf-8"
+	end
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
