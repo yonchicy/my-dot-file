4eee437cc3601dcb0adba6dec61486ebc522ac47
yonchicy <1079279184@qq.com>
2023-06-09 19:12:52 +0800

    update nvtree config for nvchad

--- nvims/nvchad/configs/overrides.lua
+++ nvims/nvchad/configs/overrides.lua
@@ -89,8 +89,18 @@ M.mason = {
     "codelldb",
 	},
 }
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
 
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('close dir'))
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('open'))
+
+end
 M.nvimtree = {
+	on_attach=on_attach,
   git = {
     enable = true,
   },
