c7286432f41c4f8a6c14c8c76c73e6902f1a0e33
yonchicy <1079279184@qq.com>
2023-06-09 20:21:54 +0800

    add nvtree default keymaps

--- nvims/nvchad/configs/overrides.lua
+++ nvims/nvchad/configs/overrides.lua
@@ -95,6 +95,7 @@ local function on_attach(bufnr)
   local function opts(desc)
     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
+ api.config.mappings.default_on_attach(bufnr)
   vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('close dir'))
   vim.keymap.set('n', 'l', api.node.open.edit, opts('open'))
 
