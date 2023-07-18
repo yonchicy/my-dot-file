53edcad901e5bb1622ae59edf1170a3b46e1c5a1
yonchicy <1079279184@qq.com>
2023-07-18 21:26:25 +0800

    lazyvim 更改 cpp 的 comment string 为 //

--- nvims/lazyvim/lua/plugins/treesitter.lua
+++ nvims/lazyvim/lua/plugins/treesitter.lua
@@ -1,11 +1,22 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
     opts = {
       ensure_installed = {
         "lua",
         "cpp",
         "rust",
+      },
+      context_commentstring = {
+        enable = true,
+        --enable_autocmd = false,
+        config = {
+          c = "//%s",
+          cpp = "//%s",
+        },
       },
     },
   },
