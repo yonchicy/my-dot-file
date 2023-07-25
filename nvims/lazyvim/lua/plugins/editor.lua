2b919478562664ea5b12b1aa20a25959f50474b9
yonchicy <1079279184@qq.com>
2023-07-25 14:55:44 +0800

    lazyvim add gitsigns blame line

--- nvims/lazyvim/lua/plugins/editor.lua
+++ nvims/lazyvim/lua/plugins/editor.lua
@@ -1,38 +1,50 @@
 local Util = require("lazyvim.util")
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        mappings = {
-          ["h"] = "close_node",
-          ["l"] = "open",
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            window = {
+                mappings = {
+                    ["h"] = "close_node",
+                    ["l"] = "open",
+                },
+            },
         },
-      },
     },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- disable the keymap to grep files
-      { "<leader>/", false },
-      -- change a keymap
-      { "<c-p>", Util.telescope("files"), desc = "Find Files (root dir)" },
-      { "<leader>fw", Util.telescope("live_grep"), desc = "Find words (root dir)" },
-      { "<leader>fs", Util.telescope("lsp_dynamic_workspace_symbols"), desc = "Find symbols" },
-      -- add a keymap to browse plugin files
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            -- disable the keymap to grep files
+            { "<leader>/", false },
+            -- change a keymap
+            { "<c-p>", Util.telescope("files"), desc = "Find Files (root dir)" },
+            { "<leader>fw", Util.telescope("live_grep"), desc = "Find words (root dir)" },
+            { "<leader>fs", Util.telescope("lsp_dynamic_workspace_symbols"), desc = "Find symbols" },
+            -- add a keymap to browse plugin files
+        },
     },
-  },
-  {
-    "echasnovski/mini.align",
-    config = function(_, opts)
-      require("mini.align").setup(opts)
-    end,
-  },
-  {
-    "Pocco81/auto-save.nvim",
-    config = function(_, opts)
-      require("auto-save").setup(opts)
-    end,
-  },
+    {
+        "echasnovski/mini.align",
+        config = function(_, opts)
+            require("mini.align").setup(opts)
+        end,
+    },
+    {
+        "Pocco81/auto-save.nvim",
+        config = function(_, opts)
+            require("auto-save").setup(opts)
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 100,
+                ignore_whitespace = true,
+            },
+        }
+    }
 }
