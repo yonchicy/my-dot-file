05019648a145c911c16397ac1837fdecefcee53c
yonchicy <1079279184@qq.com>
2023-08-10 22:35:25 +0800

    lazyvim 添加了一个在 term 窗口中跳转的快捷键

--- nvims/lazyvim/lua/config/keymaps.lua
+++ nvims/lazyvim/lua/config/keymaps.lua
@@ -86,3 +86,21 @@ vim.keymap.set("n", "<leader>ut", function()
     os.execute('sed -i "28s/light/dark/" $HOME/my-dot-file/nvims/lazyvim/lua/config/options.lua')
   end
 end, { desc = "toggle theme between light and dark" })
+
+local goto_file = function ()
+    local line = vim.api.nvim_get_current_line();
+    local pattern = "^(.-):(%d+):(%d+)"
+    local file, line, column = string.match(line, pattern)
+    if file and line  and column then
+        vim.cmd("close")
+        vim.cmd("e ".. file)
+        vim.api.nvim_win_set_cursor(0, {tonumber(line),tonumber(column)})
+    end
+end
+vim.api.nvim_create_autocmd({"BufEnter","TermOpen"}, {
+    pattern= "term://*",
+    callback = function ()
+        vim.keymap.set("n","<leader>cd",goto_file,{silent=true,noremap=true,buffer=true,desc = "go to file"})
+    end
+}
+)
