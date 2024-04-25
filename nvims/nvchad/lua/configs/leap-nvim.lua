5fa254d2dafd886162c1b5ab566897ac0ca9115e
yonchicy <1079279184@qq.com>
2024-04-25 16:01:05 +0800

    update leap mappings

--- nvims/nvchad/lua/configs/leap-nvim.lua
+++ nvims/nvchad/lua/configs/leap-nvim.lua
@@ -6,5 +6,8 @@ if not present then
 end
 
 
-leap.add_default_mappings(true)
+--leap.add_default_mappings(true)
 
+vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
+vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
