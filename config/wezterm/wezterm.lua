66b13e988540db9084decc29fa783f64827ed90f
杨士欣 <yonchicy@mac-mini.local>
2024-11-15 18:32:41 +0800

    add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Github Dark (Gogh)'
+  else
+    return 'Github Light (Gogh)'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
+config.font_size = 14
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "TITLE | RESIZE"
+
+config.default_cursor_style = 'BlinkingBlock'
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+-- and finally, return the configuration to wezterm
+return config
