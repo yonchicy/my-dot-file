92c943925de6ccd0d887f1ba54ea83a824bb8aee
yonchicy <1079279184@qq.com>
2024-12-05 14:49:06 +0800

    update ui intergrations for dap

--- nvims/nvchad/lua/chadrc.lua
+++ nvims/nvchad/lua/chadrc.lua
@@ -2,9 +2,10 @@ local M = {}
 
 
 M.base46 = {
-  theme = "ayu_light",
-  theme_toggle = { "ayu_light", "github_dark" },
+  theme = "flexoki-light",
+  theme_toggle = { "flexoki-light", "github_dark" },
   transparency = false,
+  integrations = { "dap" },
   hl_override = {
     LspInlayHint = {
       bg = "one_bg2",
@@ -14,9 +15,16 @@ M.base46 = {
 
 M.nvdash = {
   load_on_startup = true,
+  -- buttons = {
+  --   { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+  --   { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+  -- { txt = "  Recent Files", keys = "Spc q s",  cmd = "lua require('persistence').load() " },
+  -- more... check nvconfig.lua file for full list of buttons
+  -- },
 }
 M.ui = {
 
-  telescope = { style = "bordered" },    -- borderless / bordered
+  telescope = { style = "bordered" }, -- borderless / bordered
 }
+
 return M
