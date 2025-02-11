84c7dbd13c07461187475f6315edcc40fc96ddf5
yonchicy <1079279184@qq.com>
2025-02-11 14:42:49 +0800

    add git sign config

--- nvims/lazyvim/lua/plugins/core.lua
+++ nvims/lazyvim/lua/plugins/core.lua
@@ -9,4 +9,18 @@ return {
     "neovim/nvim-lspconfig",
     opts = { autoformat = false },
   },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%x%X> - <summary>",
+    },
+  },
 }
