c86461b5413c3f652c761a2d1c22f59e8c0de391
yonchicy <1079279184@qq.com>
2025-11-03 16:15:46 +0800

    update fish config vi mode

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -77,3 +77,15 @@ set -x EDITOR nvim
 
 starship init fish | source
 zoxide init fish | source
+
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+bind -M insert ctrl-n down-or-search
+end
